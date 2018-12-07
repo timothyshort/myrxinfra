e22ba654d50d3c941a42c29abc2d69bc4ee552bc
Darrel <darrel@Darrells-Air.lan>
2018-12-06 20:25:52 -0500

    clean-up

--- create-my-rx-stack.sh
+++ create-my-rx-stack.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-set s3Bucket=my-rx-bucket
-set stackName=myRxTestStack
+set -ex
+
+export s3Bucket="my-rx-bucket"
+export stackName="myRxTestStack"
 
 if aws s3 ls "s3://${s3Bucket}" 2>&1 | grep -q 'NoSuchBucket'
 then
@@ -14,9 +16,14 @@ stackStatus=$(aws cloudformation describe-stacks)
 
 if [[ $stackStatus == *"${stackName}"* ]]; then
   aws cloudformation delete-stack --stack-name ${stackName}
+  echo "Deleting Stack $stackName..."
   aws cloudformation wait stack-delete-complete --stack-name ${stackName}
+  echo "Finished Deleting Stack $stackName"
 fi
 
-aws cloudformation create-stack --stack-name ${stackName} --template-url https://s3.amazonaws.com/${s3Bucket}/templates/infrastructure-main.yaml --parameters file://params/infrastructure-main-params.json --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation validate-template --template-body "file://templates/infrastructure-main.yaml"
 
+aws cloudformation create-stack --stack-name ${stackName} --template-url "https://s3.amazonaws.com/${s3Bucket}/templates/infrastructure-main.yaml" --parameters file://params/infrastructure-main-params.json --capabilities CAPABILITY_NAMED_IAM
+echo "Creating Stack $stackName..."
 aws cloudformation wait stack-create-complete --stack-name ${stackName}
+echo "Finished Creating Stack $stackName!"
